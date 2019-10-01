9eecdafc9eb97508ecd7a99fae8cc9d53b26a5b4
kimminhyeok13@gmail.com <kmminhyeok13@gmail.com>
2019-10-01 22:28:41 +0900

    packet split

---
+++ packet_parser.py
@@ -0,0 +1,93 @@
+
+packet_list = []
+
+
+
+
+class Pcap_Parser:
+    ''' PCAP FILE FORMAT PARSER '''
+
+    def __init__(self):
+        self.data = file_open()
+        self.data_size = len(self.data)
+        self.packet_data = self.data[24:]
+
+
+        self.global_header = {
+            'magic_number': self.data[0:4],
+            'version_major': self.data[4:6],
+            'version_minor': self.data[6:8],
+            'thiszone': self.data[8:12],
+            'sigfigs': self.data[12:16],
+            'snaplen': self.data[16:20],
+            'network': self.data[20:24],
+        }
+
+
+    def print_GH(self):
+        print("--------------GLOBAL HEADER-------------")
+        for key, value in self.global_header.items():
+            print("{0} : {1} ".format(key, value))
+
+
+class Packet_Parser:
+
+    def __init__(self,pcap_data):
+        self.packet_header = {
+            'ts_sec': pcap_data[0:4],
+            'ts_usec': pcap_data[4:8],
+            'incl_len': pcap_data[8:12],
+            'orig_len': pcap_data[12:16]
+        }
+        self.packet_size = little_endian(self.packet_header['orig_len'])
+        self.packet_buff = pcap_data[16:16+self.packet_size]
+        self.next_header = pcap_data[16+self.packet_size:]
+
+    def print_PH(self):
+        print("--------------PACKET HEADER-------------")
+        for key, value in self.packet_header.items():
+            print("{0} : {1} ".format(key, value))
+
+
+
+def little_endian(value_list):
+    tmp = ''
+    for i in reversed(value_list):
+        tmp += i
+
+    return int(tmp, 16)
+
+
+
+
+def file_open():
+    filename = input("input filename : ")
+    # fp = open(filename,"rb")
+    fp = open("test.pcap", "rb")
+    pcap_data = fp.read()
+    hex_data = ['{:02x}'.format(i) for i in pcap_data]
+    # print(hex_data)
+    return hex_data
+
+
+
+def pcap2packet():
+
+    pcap = Pcap_Parser()                # Pcap 파일 내부의 패킷 파싱
+    pcap.print_GH()                     # pcap의 글로벌 헤더 출력
+    total_len = pcap.data_size - 24     # 글로벌 헤더 제외한 패킷들의 총 길이
+    packet_len = 0                      # total_len과 비교하기위한 패킷 길이
+    data = pcap.packet_data             # 글로벌 헤더 제외한 실제 패킷들의 데이터(패킷헤더 포함)
+
+    while total_len > packet_len:       # 반복문을 돌려 패킷헤더 + 패킷데이터로 구분지어 packet_list에 저장
+        packet = Packet_Parser(data)
+        packet.print_PH()
+        packet_list.append(packet)
+        data = packet.next_header
+        packet_len += (packet.packet_size+16)
+
+
+
+
+pcap2packet()
+print(len(packet_list))
--- total.py
+++ total.py
@@ -6,6 +6,16 @@ class PacketList:
         self.packet_list.append(packet)
 
 
+class Packet_Data:
+    def __init__(self,pcap_data):
+        self.packet_header = {
+            'ts_sec': pcap_data[0:4],
+            'ts_usec': pcap_data[4:8],
+            'incl_len': pcap_data[8:12],
+            'orig_len': pcap_data[12:16]
+        }
+
+
 
 class Packet_Parser:
 
@@ -13,10 +23,10 @@ class Packet_Parser:
         filename = input("input filename : ")
         #fp = open(filename,"rb")
         fp = open("test.pcap","rb")
-        pcap_data = fp.read()
-        hex_data = ['0x'+'{:02x}'.format(i) for i in pcap_data]
+        self.pcap_data = fp.read()
+        self.hex_data = ['{:02x}'.format(i) for i in self.pcap_data]
         #print(hex_data)
-        return hex_data
+        return self.hex_data
 
 
 #main()
@@ -43,24 +53,30 @@ class Pcap_Parser:
             'snaplen': self.pcap_data[16:20],
             'network': self.pcap_data[20:24],
         }
-        self.packet_header = {
-            'ts_sec': self.pcap_data[24:28],
-            'ts_usec': self.pcap_data[28:32],
-            'incl_len': self.pcap_data[32:36],
-            'orig_len': self.pcap_data[36:40]
-        }
+        # self.packet_header = {
+        #     'ts_sec': self.pcap_data[24:28],
+        #     'ts_usec': self.pcap_data[28:32],
+        #     'incl_len': self.pcap_data[32:36],
+        #     'orig_len': self.pcap_data[36:40]
+        # }
 
     def print_GH(self):
         print("--------------GLOBAL HEADER-------------")
         for key, value in self.global_header.items():
             print("{0} : {1} ".format(key, value))
 
-    def print_PH(self):
-        print("--------------PACKET HEADER-------------")
-        for key, value in self.packet_header.items():
-            print("{0} : {1}".format(key, value))
+    # def print_PH(self):
+    #     print("--------------PACKET HEADER-------------")
+    #     for key, value in self.packet_header.items():
+    #         print("{0} : {1}".format(key, value))
 
-    # def calc_values(self,key,value):
+    def calc_little_endian(self,arg1):
+        self.little_endian = ''
+        for i in reversed(arg1):
+            self.little_endian += i
+
+        return int(self.little_endian,16)
+
 
 
 
@@ -68,4 +84,8 @@ pl  = PacketList()
 p=Pcap_Parser()
 p.print_GH()
 p.print_PH()
+print(p.calc_little_endian(p.global_header['snaplen']))
+
+
+
 
